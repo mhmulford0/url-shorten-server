6f2c2053b5c8c9525952a6967af54721f7656f4b
Matthew Mulford <mhmulford0@gmail.com>
2020-11-05 09:51:02 -0600

    added url generation

--- routes/urlRouter.js
+++ routes/urlRouter.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const nanoid = require("nanoid");
+
 
 router.get("/", (req, res) => {
   res.status(200).json({ message: "url routes" });
+});
+
+router.post("/", async (req, res) => {
+  const { longUrl } = req.body;
+  const id = nanoid.nanoid(14);
+  res.status(201).json({ url_added: id });
 });
 
 module.exports = router;
