270102e8ddae5a67cfebc9b57edd87d7d6fad8df
Matthew Mulford <mhmulford0@gmail.com>
2020-11-28 16:01:43 -0600

    fixed date formatting

--- routes/urlRouter.js
+++ routes/urlRouter.js
@@ -1,12 +1,12 @@
-const router = require("express").Router();
-const nanoid = require("nanoid");
+const router = require('express').Router();
+const nanoid = require('nanoid');
 const db = require('../data/dbConfig');
 
 const validUrl = require('valid-url');
 const publicIp = require('public-ip');
 const axios = require('axios');
 
-router.get("/", (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).json({ message: 'API up and running' });
 });
 
@@ -18,13 +18,12 @@ router.get('/:shortLink', async (req, res) => {
       vistorIp = await publicIp.v4();
     } catch (error) {
       console.log(error);
-      vistorIp = '8.8.8.8'
+      vistorIp = '8.8.8.8';
     }
-    
   } else {
-    vistorIp = req.headers['x-forwarded-for'] || "8.8.8.8";
+    vistorIp = req.headers['x-forwarded-for'] || '8.8.8.8';
   }
-  
+
   try {
     const linkId = await db('links')
       .where('shortLink', '=', shortLink)
@@ -42,7 +41,7 @@ router.get('/:shortLink', async (req, res) => {
       await db('click_info').insert({
         location: locationData,
         link_id: link.id,
-        click_date: new Date().toISOString(),
+        click_date: new Date().toDateString(),
       });
 
       res.redirect(link.longLink);
@@ -55,47 +54,46 @@ router.get('/:shortLink', async (req, res) => {
   }
 });
 
-
 router.get('/:shortLink/info', async (req, res) => {
   const shortLink = req.params.shortLink;
-  try{
-      const data = await db('click_info')
-        .join('links', 'links.id', '=', 'click_info.link_id')
-        .select(
-          'click_info.id',
-          'click_info.location',
-          'click_info.click_date',
-          'links.longLink',
-          'links.shortLink'
-        )
-        .where('links.shortLink', shortLink)
-        .orderBy('click_info.click_date', 'asc');
-      const linkData = data.map((link) => {
-        return { longLink: link.longLink, shortLink: link.shortLink };
-      });
-      const clickData = data.map((click) => {
-        return { location: click.location, date: click.click_date };
-      });
+  try {
+    const data = await db('click_info')
+      .join('links', 'links.id', '=', 'click_info.link_id')
+      .select(
+        'click_info.id',
+        'click_info.location',
+        'click_info.click_date',
+        'links.longLink',
+        'links.shortLink'
+      )
+      .where('links.shortLink', shortLink)
+      .orderBy('click_info.click_date', 'asc');
+    const linkData = data.map((link) => {
+      return { longLink: link.longLink, shortLink: link.shortLink };
+    });
+    const clickData = data.map((click) => {
+      return { location: click.location, date: click.click_date };
+    });
 
-      if(!clickData || !linkData){
-        res.status(400).json({message: "Link Not found"})
-      } else {
-          res.status(200).json({ linkInfo: linkData[0], clickInfo: clickData });
-      }
-    } 
-    catch (error) {
-      console.log(error)
-      res.status(500).json({message: "server error"})
+    if (!clickData || !linkData) {
+      res.status(400).json({ message: 'Link Not found' });
+    } else {
+      res.status(200).json({ linkInfo: linkData[0], clickInfo: clickData });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'server error' });
   }
-
-})
-
+});
 
 router.post('/', async (req, res) => {
   const { longLink } = req.body;
-  const shortLink = nanoid.customAlphabet('1234567890abcdefghijklmnopqrstuvwxyz', 12)();
-  
-  if(validUrl.isWebUri(longLink)) {
+  const shortLink = nanoid.customAlphabet(
+    '1234567890abcdefghijklmnopqrstuvwxyz',
+    12
+  )();
+
+  if (validUrl.isWebUri(longLink)) {
     try {
       await db('links').insert({
         longLink: longLink,
@@ -104,14 +102,12 @@ router.post('/', async (req, res) => {
 
       res.status(201).json({ message: shortLink });
     } catch (error) {
-      console.log(error)
-      res.status(500).json({message: "there was an error with your request"})
+      console.log(error);
+      res.status(500).json({ message: 'there was an error with your request' });
     }
-
   } else {
-    res.status(400).json({message: "invalid URL"})
+    res.status(400).json({ message: 'invalid URL' });
   }
-  
 });
 
 module.exports = router;
