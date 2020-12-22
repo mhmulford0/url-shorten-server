e17e368dd8e38e1a14d13f9ae5be6da9eed4c14b
Matthew Mulford <mhmulford0@gmail.com>
2020-12-22 10:37:28 -0600

    deploy prep

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 const corsOptions = {
   origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200, 
 };
 
 const urlRouter = require("./routes/urlRouter");
