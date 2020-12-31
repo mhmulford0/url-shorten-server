c69d21b44b8a463c3d0d7ed00e8deb956a3d2224
Matthew Mulford <mhmulford0@gmail.com>
2020-12-31 13:42:20 -0600

    updated callback url for google login, changed index redirect

--- routes/urlRouter.js
+++ routes/urlRouter.js
@@ -7,9 +7,9 @@ const publicIp = require('public-ip');
 const axios = require('axios');
 
 router.get('/', (req, res) => {
-  //res.redirect('https://client.lnkshrt.app/')
-  console.log(req.user)
-  res.sendStatus(201).end()
+  res.redirect('https://client.lnkshrt.app/')
+  
+  
 });
 
 router.get('/:shortLink', async (req, res) => {
