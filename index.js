4780bb6fffd85aa2331491491fc5656ab0536913
Matthew Mulford <mhmulford0@gmail.com>
2020-12-31 14:06:51 -0600

    updated cookie settings

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const corsOptions = {
 app.use(cookieSession({
   name: "lnkshrtapp",
   keys: ['erghi923902309923u9023'],
-  domain: 'client.lnkshrt.app'
+  domain: 'lnkshrt.app'
 }))
 app.use(cookieParser());
 app.use(passport.initialize());
